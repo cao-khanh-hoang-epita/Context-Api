8a0d7821d70919116743944d8ccd9bc65f104707
Khanh <hoangcaokhanhhn2004@gmail.com>
2024-12-06 12:27:10 +0100

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.jsx"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import CountContextProvider from "./context/CountContextProvider"; // Import the provider
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-)
+  <StrictMode>
+    <CountContextProvider>
+      <App />
+    </CountContextProvider>
+  </StrictMode>
+);
